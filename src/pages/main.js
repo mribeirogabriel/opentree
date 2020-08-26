ff00ed5b8a37581440c2c5bdd8d789c2c9a5a078
marco <mribeirogabriel@gmail.com>
2020-08-26 17:29:45 -0300

    first commit

---
+++ src/pages/main.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import '../assets/css/main.css'
+import photoProfile from '../assets/images/profile.png'
+
+import instagramLogo from '../assets/images/instagram.svg'
+import twitterLogo from '../assets/images/twitter.svg'
+import youtubeLogo from '../assets/images/youtube.svg'
+import twitchLogo from '../assets/images/twitch.svg'
+import facebookLogo from '../assets/images/facebook.svg'
+
+const pageTitle = 'Name | Social Media Links'
+const profileName = 'Profile Name'
+
+const linkList = {
+    'Instagram': ['https://instagram.com', instagramLogo],
+    'Twitter': ['https://twitter.com', twitterLogo],
+    'Youtube': ['https://youtube.com', youtubeLogo],
+    'Twitch': ['https://twitch.com', twitchLogo],
+    'Facebook':['https://facebook.com', facebookLogo]
+}
+const links = []
+const logos = []
+function loadLinks(){
+    for (const socialMedia in linkList) {
+        const mediaLink = linkList[socialMedia][0]
+        const logoLink = linkList[socialMedia][1]
+        links.push(<div key={socialMedia} id='link'><a href={mediaLink} target='_blank' rel="noopener noreferrer">{socialMedia}</a></div>)
+        logos.push(<a key={mediaLink} href={mediaLink} target='_blank' rel="noopener noreferrer"><img src={logoLink}></img></a>)
+    }
+}
+loadLinks()
+function Main(){
+    return(
+        <>
+        <title>{pageTitle}</title>
+        <div id='main-container'>
+            <div id='main-photo'>
+                <img src={photoProfile}></img>
+                <h3>{profileName}</h3>
+            </div>
+            <div id='main-links'>
+                {links}
+            </div>
+            <div id='main-media'>
+                {logos}
+            </div>
+            <div id='main-footer'>
+                FOOTER
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Main
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Main from './pages/main'
+
+function Routes(){
+    return(
+        <BrowserRouter>
+            <Route path='/' exact component={Main}/>
+        </BrowserRouter>
+    )
+}
+
+export default Routes
